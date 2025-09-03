faae5f0b665e28b0132d3ed42c5fde2ce319550e
Darrin <dkfhui@gmail.com>
2025-09-03 00:43:09 -0500

    Added GET request methods from Ktor tutorial

--- src/main/kotlin/Routing.kt
+++ src/main/kotlin/Routing.kt
@@ -1,6 +1,8 @@
 package com.dkfhui
 
+import com.dkfhui.model.*
 import io.ktor.http.ContentType
+import io.ktor.http.HttpStatusCode
 import io.ktor.server.application.*
 import io.ktor.server.http.content.staticResources
 import io.ktor.server.plugins.statuspages.StatusPages
@@ -23,6 +25,41 @@ fun Application.configureRouting() {
 
         get("/errortest") {
             throw IllegalStateException("This is a test page")
+        }
+
+        get("/tasks") {
+            val tasks = TaskRepository.allTasks()
+            call.respondText(
+                contentType = ContentType.parse("text/html"),
+                text = tasks.tasksAsTable()
+            )
+        }
+
+        get("/tasks/byPriority/{priority?}") {
+            val priorityString = call.parameters["priority"]
+
+            if (priorityString == null) {
+                call.respond(HttpStatusCode.BadRequest)
+                return@get
+            }
+
+            try {
+                val priority = Priority.valueOf(priorityString)
+                val tasks = TaskRepository.tasksByPriority(priority)
+
+                if(tasks.isEmpty()) {
+                    call.respond(HttpStatusCode.NotFound)
+                    return@get
+                }
+
+                call.respondText(
+                    contentType = ContentType.parse("text/html"),
+                    text = tasks.tasksAsTable()
+                )
+            }
+            catch (ex: java.lang.IllegalArgumentException) {
+                call.respond(HttpStatusCode.BadRequest)
+            }
         }
     }
 
---
+++ src/test/kotlin/ApplicationTest.kt
@@ -0,0 +1,70 @@
+import com.dkfhui.module
+import io.ktor.client.request.get
+import io.ktor.client.statement.bodyAsText
+import io.ktor.http.HttpStatusCode
+import io.ktor.http.contentType
+import io.ktor.server.testing.testApplication
+import org.junit.Assert
+import org.junit.Test
+import kotlin.test.assertContains
+import kotlin.test.assertEquals
+
+class ApplicationTest {
+    @Test
+    fun testRoot() = testApplication {
+        application {
+            module()
+        }
+        val response = client.get("/")
+
+        Assert.assertEquals(HttpStatusCode.Companion.OK, response.status)
+        Assert.assertEquals("Hello World!", response.bodyAsText())
+    }
+
+    @Test
+    fun testNewEndpoint() = testApplication {
+        application {
+            module()
+        }
+
+        val response = client.get("/test1")
+
+        Assert.assertEquals(HttpStatusCode.Companion.OK, response.status)
+        Assert.assertEquals("html", response.contentType()?.contentSubtype)
+        assertContains(response.bodyAsText(), "Hello From Ktor")
+    }
+
+    @Test
+    fun tasksCanBeFoundByPriority() = testApplication {
+        application {
+            module()
+        }
+
+        val response = client.get("/tasks/byPriority/Medium")
+        val body = response.bodyAsText()
+
+        assertEquals(HttpStatusCode.OK, response.status)
+        assertContains(body, "Mow the lawn")
+        assertContains(body, "Paint the fence")
+    }
+
+    @Test
+    fun invalidPriorityProduces400() = testApplication {
+        application {
+            module()
+        }
+
+        val response = client.get("/tasks/byPriority/Invalid")
+        assertEquals(HttpStatusCode.BadRequest, response.status)
+    }
+
+    @Test
+    fun unusedPriorityProduces404() = testApplication {
+        application {
+            module()
+        }
+
+        val response = client.get("/tasks/byPriority/Vital")
+        assertEquals(HttpStatusCode.NotFound, response.status)
+    }
+}
